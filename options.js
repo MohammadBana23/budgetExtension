18b3731001339282d29c3816ed968366481de8f5
mohammad <mohamad.bana.1380@gmail.com>
2021-01-15 03:59:12 -0800

    show notifications

--- options.js
+++ options.js
@@ -1,20 +1,30 @@
-$(function(){
+$(function () {
 
-  chrome.storage.sync.get('limit',function(budget){
-      $('#limit').val(budget.limit);
-  })  
+    chrome.storage.sync.get('limit', function (budget) {
+        $('#limit').val(budget.limit);
+    })
 
-    $('#saveLimit').click(function(){
+    $('#saveLimit').click(function () {
         var limit = $('#limit').val();
-        if(limit){
-            chrome.storage.sync.set({'limit': limit},function(){
+        if (limit) {
+            chrome.storage.sync.set({ 'limit': limit }, function () {
                 close();
             });
         }
     });
 
-    $('#resetTotal').click(function(){
-        chrome.storage.sync.set({'total': 0});
-    })
-    
+    $('#resetTotal').click(function () {
+        chrome.storage.sync.set({ 'total': 0 }, function () {
+            var notifOptions = {
+                type: 'basic',
+                iconUrl: 'algodaric64.png',
+                title: 'Total reset!',
+                message: "Total has been reset to 0!"
+            };
+            chrome.notifications.create('limitNotif', notifOptions);
+        });
+    });
 });
+
+
+
--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ $(function(){
     })
 
     $('#spendAmount').click(function(){
-        chrome.storage.sync.get('total',function(budget){
+        chrome.storage.sync.get(['total','limit'],function(budget){
             var newTotal = 0;
             if (budget.total){
                 newTotal += parseInt(budget.total);
@@ -17,7 +17,19 @@ $(function(){
                 newTotal += parseInt(amount);
             }
 
-            chrome.storage.sync.set({'total': newTotal});
+            chrome.storage.sync.set({'total': newTotal},function(){
+                if(amount && newTotal >= budget.limit){
+                    var notifOptions = {
+                        type : 'basic',
+                        iconUrl : 'algodaric64.png',
+                        title : 'Limit reached!',
+                        message : "Uh oh! Looks like you've reached your limit"
+                    };
+                    chrome.notifications.create('limitNotification',notifOptions);
+                }
+            });
+
+
             $('#total').text(newTotal);
             $('#amount').val('');
 
